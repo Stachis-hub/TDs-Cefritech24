fa8fa8178fcd0d132f4c32a5e024781efa27460e
barakeel <njaloungbarakeel@gmail.com>
2024-07-29 15:28:54 +0100

    modification

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
     <body>
         <div class="container mt-4">
+        <?php  ?>
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
@@ -35,10 +36,9 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php
+                                <?php
                                     $query = "select* from student"; //ici on recherche les etudiant dans la table
                                     $query_run = mysqli_query($con, $query);
-
                                     if (mysqli_num_rows($query_run) > 0) {
                                         foreach($query_run as $student){ //ici on récupère ce qui est dans chaque étudiants et on l'affiche
                                             ?>
@@ -53,6 +53,11 @@
                                                 <a href=""> voir </a>
                                                 <a href=""> éditer </a>
                                                 <a href=""> supprimer </a>
+                                                <form action = "code.php" method="POST" class="d-inline">
+                                                    <button type="submit" name="delete_student" value="<?= $student['id']; ?>" class="btn btn-danger btn-sm">
+                                                        Supprimer
+                                                    </button>
+                                                </form>
                                             </td>
                                             </tr>
                                             <?php 
@@ -63,6 +68,7 @@
                                             
                                         
                                     ?>
+                               
                                 </tbody>
                             </table>
                         </div>
